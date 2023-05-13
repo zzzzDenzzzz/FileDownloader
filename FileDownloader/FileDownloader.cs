a7fdc709dec25ddd3b14f16a1f18cf793cb4c580
dell <dell@DESKTOP-LEDM7C3>
2023-05-13 09:01:56 +0300

    Добавьте файлы проекта.

---
+++ FileDownloader/FileDownloader.cs
@@ -0,0 +1,55 @@
+﻿namespace FileDownloader
+{
+    internal class FileDownloader
+    {
+        public static async Task DownloadFile(string uri, IProgress<double> progress)
+        {
+            ArgumentNullException.ThrowIfNull(uri);
+            if (!Uri.TryCreate(uri, UriKind.Absolute, out var uriResult))
+            {
+                throw new ArgumentException("Invalid uri", nameof(uri));
+            }
+
+            using var client = new HttpClient();
+            // считываем заголовки
+            using var response = await client.GetAsync(
+                uriResult, HttpCompletionOption.ResponseHeadersRead);
+
+            var contentLength = response.Content.Headers.ContentLength;
+
+            await using var contentStream = await response.Content.ReadAsStreamAsync();
+
+            byte[] buffer = new byte[8192];
+            var totalBytesRead = 0L;
+            await using var file = File.OpenWrite("file.zip");
+
+            while (true)
+            {
+                int countOfBytesRead = await contentStream.ReadAsync(buffer);
+                if (countOfBytesRead == 0)
+                {
+                    break;
+                }
+                Console.WriteLine(countOfBytesRead);
+
+                if (countOfBytesRead == buffer.Length)
+                {
+                    // запись буфера в файл
+                    await file.WriteAsync(buffer);
+                }
+                else
+                {
+                    var bytesRead = buffer[..countOfBytesRead];
+                    // запись буфера в файл
+                    await file.WriteAsync(bytesRead);
+                }
+                totalBytesRead += countOfBytesRead;
+
+                if (contentLength != null)
+                {
+                    progress.Report((double)totalBytesRead / contentLength.Value);
+                }
+            }
+        }
+    }
+}
---
+++ FileDownloader/Program.cs
@@ -0,0 +1,8 @@
+﻿var progress = new Progress<double>(p =>
+{
+    Console.WriteLine(Math.Round(p, 2));
+});
+
+await FileDownloader.FileDownloader.DownloadFile(
+    "https://github.com/rodion-m/SystemProgrammingCourse2022/raw/master/files/payments_19mb.zip",
+    progress);
